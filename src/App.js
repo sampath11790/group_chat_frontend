2410f3b3c298fd7e83dfe1cd631ddc8e2c4bbb6d
sampath11790 <bsampath11790@gmail.com>
2023-06-18 15:28:30 +0530

    react router configuration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import CreateGroup from "./components/chatPage/CreateGroup/CreateGroup";
 import ChatMain from "./components/chatPage/chatMain";
 import SignupPage from "./components/login/Signup";
@@ -7,8 +8,13 @@ function App() {
   return (
     <div>
       {/* <SignupPage></SignupPage> */}
-      <ChatMain></ChatMain>
+      {/* <ChatMain></ChatMain> */}
       {/* <CreateGroup></CreateGroup> */}
+      <Routes>
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="chatmain" element={<ChatMain />} />
+        <Route path="creategroup" element={<CreateGroup />} />
+      </Routes>
     </div>
   );
 }
